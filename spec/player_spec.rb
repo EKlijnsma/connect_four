c1cad878e2d09ca596f4533fc909f84ebc08d5a1
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-08-26 11:52:11 +0200

    update specs for get_move method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -11,4 +11,20 @@ describe Player do
       expect(player.symbol).to eq(symbol)
     end
   end
+
+  describe '#get_move' do
+    it 'prompts for user input' do
+      expect(player).to receive(:get_input)
+    end
+
+    it 'verifies the user input' do
+      expect(player).to receive(:verify_input)
+    end
+
+    it 'returns valid user input' do
+      allow(player).to receive(:get_input).and_return('2')
+      allow(player).to receive(:verify_input).and_return(2)
+      expect(player.get_move).to eq(2)
+    end
+  end
 end
